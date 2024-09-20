2cec179288b7044a5bfb8c30e9114216638f1c94
Fabia Herrera <herrerafabia.190604@gmail.com>
2024-09-19 20:19:44 -0500

    feat(login): activity added

--- app/src/main/java/com/example/airlines/MainActivity.kt
+++ app/src/main/java/com/example/airlines/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.example.airlines
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        android.os.Handler(Looper.getMainLooper()).postDelayed({
+            // Navegar a otra Activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Opcional, si no quieres que el usuario pueda volver atrás
+        }, 3000)
     }
 }
